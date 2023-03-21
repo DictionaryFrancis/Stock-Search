c510d614932026c208ae0550df343689290b9ae8
Francis Matias <fwmatias64@gmail.com>
2023-03-21 14:04:58 +0000

    adding basic JS

---
+++ index.js
@@ -0,0 +1,8 @@
+let stockNameElem = document.querySelector("#stock_name");
+let stockPriceElem = document.querySelector("#price_id");
+let stockLetterElem = document.querySelector("#stock_letters");
+
+stockNameElem.textContent = "BlackBerry"
+stockPriceElem.textContent = "$3.6660"
+stockLetterElem.textContent = "BB"
+
