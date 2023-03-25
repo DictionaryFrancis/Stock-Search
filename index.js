41ff39cee7866806ac5ff710a34244f1f22d2c9f
Francis Matias <fwmatias64@gmail.com>
2023-03-25 13:56:10 +0000

    Creating functions and add API

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
-let stockNameElem = document.querySelector("#stock_name");
-let stockPriceElem = document.querySelector("#price_id");
-let stockLetterElem = document.querySelector("#stock_letters");
+const stockNameElem = document.querySelector("#stock_name");
+const stockPriceElem = document.querySelector("#price_id");
+const stockLetterElem = document.querySelector("#stock_letters");
+const search_btn = document.querySelector(".btn_search");
+const container_info = document.querySelector(".container_info");
+const interval = "5min";
+const errorElem = document.querySelector(".error")
 
-stockNameElem.textContent = "BlackBerry"
-stockPriceElem.textContent = "$3.6660"
-stockLetterElem.textContent = "BB"
+
+
+search_btn.addEventListener("click", function () {
+    const input_symbol = document.getElementById("input_search").value
+    console.log(input_symbol)
+
+    const symbolGenerated = input_symbol
+    console.log(symbolGenerated)
+
+
+
+    const apikey = "VPG1MAVCBEQPROPT";
+    const symbol = symbolGenerated;
+    //const interval = "5min";
+
+    const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${apikey}`;
+
+
+    //let BB = {
+    //    sotckName:"BlackBerry",
+    //    price: 35.065,
+    //    stockLetter:"BB"
+    //}
+
+    //stockNameElem.textContent = "BlackBerry"
+    //stockPriceElem.textContent = "$3.6660"
+    //stockLetterElem.textContent = "BB"
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) =>
+        
+
+
+
+            setTimeout(() => {
+                container_info.style.display = "block"
+
+                
+        if (data.Name === undefined) {
+            container_info.style.display = "none";
+            errorElem.innerHTML = `No matching results for "${symbolGenerated} "`
+            return errorElem.style.display = "block";
+        } else {
+
+                 errorElem.style.display = "none";
+                stockNameElem.textContent = `${data.Name}`
+                stockPriceElem.textContent = `$ ${data.BookValue}`
+                stockLetterElem.textContent = `${data.Symbol}`
+            }}, "1000"));
+        
+        }
+)
+    
+
 
